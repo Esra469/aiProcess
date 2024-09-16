8cc0ff18e6e9a9d0f412a2e9be6c5d6afb8c2285
Esra Çimen <eesra8352@gmail.com>
2024-09-17 01:02:50 +0300

    update machine

--- machine_learning/linear_regresyon_tutorial.py
+++ machine_learning/linear_regresyon_tutorial.py
@@ -34,3 +34,51 @@ bu şekilde olmaması için residual ın karesi alınıyır negatiflerden kurtur
 mean squared error->MSE=sum(residual^2)/n    n=sample(nokta )sayısı
 amaç->min(MSE)
 """
+
+
+#import data
+df=pd.read_csv("linear_regresion_dataset.csv",sep=";")#; ile 2 kısıma ayırdık
+#df=pd.DataFrame(data,columns=['deneyim','maas'])
+plt.scatter(df.deneyim,df.maas)
+plt.xlabel("deneyim")
+plt.ylabel("maas")
+plt.show()
+
+#%% linear regession
+
+#sklearn library
+from sklearn.linear_model import LinearRegression
+linear_reg=LinearRegression()
+#noktalar oluşturuldu
+#x=df.deneyim#şu an type olarak pandas alıyor bir kolaylık olsun diye numpy e dönüştürmeliyz ->df.deneyim.values ile numpy olur
+#x=df.deneyim.values#ama sklearn bu şekilde algılamıyor bunu (14,) olarak algılar 14,1 olarak ayarlamk lazım bunun için reshape yapıyoruz
+x=df.deneyim.values.reshape(-1,1)
+y=df.maas.values.reshape(-1,1)
+
+linear_reg.fit(x,y)#mavi noktalar fit edildi
+
+#%%prediction
+b0=linear_reg.predict(0)
+print("b0:",b0)
+
+b0_=linear_reg.intercept_
+print("b0_:",b0_)#y eksenini  kesitiği nokta
+
+b1=linear_reg.coef_
+print("b1:",b1)#eğim slope
+
+#yukarıda verdiğimiz formulleri istendiğinde bu şeklde pythondan bulabiliriz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
