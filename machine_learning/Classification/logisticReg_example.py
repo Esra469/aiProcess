f585d685a7d0e89d662c241b4522a7c5c75f5f56
Esra Çimen <eesra8352@gmail.com>
2024-09-22 12:03:29 +0300

    add classification

--- machine_learning/Classification/logisticReg_example.py
+++ machine_learning/Classification/logisticReg_example.py
@@ -51,7 +51,8 @@ x=(x_data-np.min(x_data))/(np.max(x_data)-np.min(x_data))#.values#Bu şekilde no
 #%% Dataset train and Test split
 #Train test split
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+#x_train ve y_train logistic regression için ayrıldı x_test ve y_test ile de bunlar üsütnde deneme yapılacak
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)#test_size=verilen deger yüzde kaça kaç bölündüğnü gösterir
 
 x_train=x_train.T
 x_test=x_test.T
@@ -63,9 +64,101 @@ print("x_test:",x_test.shape)
 print("y_train:",y_train.shape)
 print("y_test:",y_test.shape)
 
+#%%Parameter initialize and sigmoid Function
+def initialze_weights_and_bias(dimension):
+    w=np.full((dimension,1),0.01)#dimensionu 1 lerden oluşan bir weight matrixi yap demek 0.01 atıyoruz 0 atamayız çünkü her değeri 0 ile çarpıp 0 dönmesine neden olacak
+    b=0.0 #bias değeri
+    return w,b
+w,b=initialze_weights_and_bias(30)
 
+"""
+sigmoid Funciton -> F(x)=1/(1+e^-(x)) ->x olarak z yi yazıyoruz bu sefer
+"""
+def sigmoid(z):
+    y_head=1/(1+np.exp(-z))
+    return y_head
 
+#sigmoid(0) ->gelen değeri görüyoruz 
+#yukarıda weightlerimizi initialize ettik
 
+#implementing Forward and Backward
+def forward_backward_propagation(w,b,x_train,y_train):
+    #forward propagation
+    z=np.dot(w.T,x_train)+b
+    y_head=sigmoid(z)
+    loss=-y_train*np.log(y_head)-(1-y_train)*np.log(1-y_head)
+    cost=(np.sum(loss))/x_train.shape[1]#bir değeri sample ile bölmek değeri normalleştirmek demektir x_train.shape[1] is for scaling
+    #backward propagation
+    derivation_weight=(np.dot(x_train,((y_head-y_train).T)))/x_train.shape[1]#x_train.shape[1] is for scaling
+    derivation_bias=np.sum(y_head-y_train)/x_train.shape[1] #x_train.shape[1] is for scaling
+    gradients={"derivative_weight":derivation_weight,"derivative_bias ":derivation_bias}#Burası dictioanry ->parametreleri depolamak için kullanılıyor
+    return cost,gradients
+"""
+2 matis çarpıldığında 1. matrisin columu ve 2. matrisin row u birbirine eşit olmalı bundan dolayı genelede ilk matrisin Transpozu alınır(T) 
+"""
+
+#implementing update Parameters
+#ileri ve geri gitmek bir iterasyon demektir bu zamanda w ve b update ediliyor
+def update(w,b,x_train,y_train,learning_rate,number_of_iteration):#number_of_iterator döngünün n ekadar forward ve backward propoagto yaptğışı blirtiyor
+    cost_list=[]
+    cost_list2=[]
+    index=[]
+    
+    #updating(learning) parameters is number_of_iteration times
+    for i in range(number_of_iteration):
+        cost,gradients=forward_backward_propagation(w, b, x_train, y_train)
+        cost_list.append(cost)
+        #lets update
+        w=w-learning_rate*gradients["derivative_weight"]#weigth eşittir öğrnme hızı-learing weght a göre türevi demek aslında bu
+        b=b-learning_rate*gradients["derivative_bias"]#bias için de aynı
+        if i % 10==0:
+            cost_list2.append(cost)#her 10 adımda bir cost_list2 ye o costlar atılıyor
+            index.append(i)
+            print("Cost after iteration %i: %f"%(i,cost))
+    
+    #we update(learning ) parameteres weight and bias
+    parameteres={"weight":w,"bias":b}#w ve b paramterleri paramters de depoluyruz bu br dict
+    plt.plot(index,cost_list2)
+    plt.xticks(index,rotation='vertical')
+    plt.xlabel("Number of iteration")
+    plt.ylabel("Cost")
+    plt.show()
+    return parameteres,gradients,cost_list
+
+#implemeting prediction
+def predict(w,b,x_test):
+    #x_test is a input for forward propagation
+    z=sigmoid(np.dot(w.T,x_test)+b)#np.dot bildiğimiz matris çarpımı gibi işloyr +b de bias değeri ekliyor
+    y_prediction=np.zeros(1,x_test.shape[1])#0 lardan oluşan bir prediction oluşturuduk daha sonra bunu dolduracağız aşağıdaki if koşulunda
+    #if z is bigger than 0.5, our prediction is sign one (y_head=1),
+    #if z is smaller than 0.5, our prediction is sign zero (y_head=0),
+    for i in range(z.shape[1]):
+        if z[0,i]<=0.5:
+            y_prediction[0,i]=0
+        else:
+            y_prediction[0,i]=1
+    return y_prediction
+
+#implementing Logistic regression
+def logistic_regression(x_train,y_train,x_test,y_test,learning_rate,num_iteration):
+    #initialize
+    dimension=x_train.shape[0]
+    w,b=initialze_weights_and_bias(dimension)
+    #do not change learning rate
+    parameters,gradients,cost_list=update(w,b,x_train,y_train,learning_rate,num_iteration)
+    y_prediction_test=predict(parameters["weight"], parameters["bias"], x_test)
+    y_prediction_train=predict(parameters["weight"],parameters["bias"],x_train)
+    
+    #print train/test Errors
+    print("train accuracy: {] %".format(100-np.mean(np.abs(y_prediction_train-y_train))*100))
+    print("test accuracy: {} %".format(100-np.mean(np.abs(y_prediction_test-y_test))*100))
+    
+logistic_regression(x_train, y_train, x_test, y_test, learning_rate=1, num_iteration=100)
+
+from sklearn.linear_model import LogisticRegression
+lr=LogisticRegression()
+lr.fit(x_train.T,y_train.T)
+print("test accuracy {}".format(lr.score(x_test.T,y_test.T)))
 
 
 
